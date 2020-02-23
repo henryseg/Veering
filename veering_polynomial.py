c466d62f951395c07545a8f2ad6cbb76b58eff8e
Saul Schleimer <saulsch@gmail.com>
2020-02-23 08:25:15 +0000

    blacken veering_polynomial

--- veering_polynomial.py
+++ veering_polynomial.py
@@ -2,8 +2,9 @@
 # veering_polynomial.py
 #
 
-# Goal - compute the "big" and "small" veering polynomials as defined
-# by Sam Taylor et al.
+# compute the "big" and "small" veering polynomials as defined by Sam
+# Taylor et al.  We also compute the alexander polynomial here, to
+# ensure that we are using the same spanning tree.
 
 import regina
 
@@ -20,9 +21,9 @@ from veering import is_veering
 
 verbose = 0
 
+
 # Polynomials that come with snappy (in sage) - delete these or move
 # them to the correct place.
-
 
 def alex_is_monic(M):
     p = M.alexander_polynomial()
@@ -37,7 +38,6 @@ def hyper_is_monic(M): # add a way to dial up the precision
 
 # veering structure
 
-
 edge_vert_index_map = {(0, 1):0, (0, 2):1, (0, 3):2, (1, 2): 3, (1, 3):4, (2, 3):5 }
 
 
@@ -50,53 +50,66 @@ def tet_lower_upper_edges(tetrahedron, coorientations):
     return ( tetrahedron.face(1, lower_edge_num), tetrahedron.face(1, upper_edge_num) )
 
 
-### computing the big veering polynomial
-
+# computing the big veering polynomial
 
 def has_red_lower_edge(tetrahedron, coorientations, edge_colours):
     lower_edge = tet_lower_upper_edges(tetrahedron, coorientations)[0]
-    return edge_colours[lower_edge.index()] == 'R'
+    return edge_colours[lower_edge.index()] == "R"
 
 
 @liberal
 def edges_to_tetrahedra_matrix(triangulation, angle_structure, ZH, P):
-    coorientations = is_transverse_taut(triangulation, angle_structure, return_type = 'tet_vert_coorientations')
-    if verbose > 0: print 'coorientations', coorientations
-    edge_colours = is_veering(triangulation, angle_structure, return_type = 'veering_colours')
-    if verbose > 0: print 'edge_colours', edge_colours
+    coorientations = is_transverse_taut(triangulation, angle_structure, return_type = "tet_vert_coorientations")
+    if verbose > 0:
+        print "coorientations", coorientations
+    edge_colours = is_veering(triangulation, angle_structure, return_type = "veering_colours")
+    if verbose > 0:
+        print "edge_colours", edge_colours
     red_tetrahedra = []
     blue_tetrahedra = []
+
     for tet in triangulation.tetrahedra():
         if has_red_lower_edge(tet, coorientations, edge_colours):
             red_tetrahedra.append(tet)
         else:
             blue_tetrahedra.append(tet)
-    if verbose > 0: print 'how many reds and blues', len(red_tetrahedra), len(blue_tetrahedra)
+    if verbose > 0:
+        print "how many reds and blues", len(red_tetrahedra), len(blue_tetrahedra)
 
     face_laurents = faces_in_laurent(triangulation, angle_structure, ZH)
-    if verbose > 0: print 'face_laurents', face_laurents
+    if verbose > 0:
+        print "face_laurents", face_laurents
 
     ET_matrix = [] # now to find the tet coefficients relative to each edge
     for tet in triangulation.tetrahedra():
-        if verbose > 0: print 'tet_index', tet.index()
+        if verbose > 0:
+            print "tet_index", tet.index()
         edge = tet_lower_upper_edges(tet, coorientations)[1]
-        if verbose > 0: print 'edge_index', edge.index()
+        if verbose > 0:
+            print "edge_index", edge.index()
         edge_colour = edge_colours[edge.index()]
-        if verbose > 0: print 'edge_colour', edge_colour
+        if verbose > 0:
+            print "edge_colour", edge_colour
         embeddings = edge.embeddings()
         tet_coeffs = [ZH(0)] * triangulation.countTetrahedra()
         tet_coeffs[tet.index()] = 1  # bottom tet around the edge gets a 1
-        if verbose > 0: print 'initial tet_coeffs', tet_coeffs
+        if verbose > 0:
+            print "initial tet_coeffs", tet_coeffs
         current_coeff = ZH(1)
+
         # find index of bottom embedding in the list of embedding
         for i, embed in enumerate(embeddings):
             tet = embed.tetrahedron()
-            if verbose > 0: print 'current_tet', tet.index()
+            if verbose > 0:
+                print "current_tet", tet.index()
             vert_perm = embed.vertices()
             trailing_vert_num, leading_vert_num = vert_perm[2], vert_perm[3]
-            if coorientations[tet.index()][trailing_vert_num] == +1 and coorientations[tet.index()][leading_vert_num] == +1:
+
+            if (coorientations[tet.index()][trailing_vert_num] == +1 and
+                coorientations[tet.index()][leading_vert_num]  == +1):
                 bottom_index = i
                 break
+
         embeddings = embeddings[bottom_index:] + embeddings[:bottom_index]
         sign = 1 # we are going up the left side of the edge
         for embed in embeddings[1:]:  # skipping the first
@@ -104,13 +117,17 @@ def edges_to_tetrahedra_matrix(triangulation, angle_structure, ZH, P):
             vert_perm = embed.vertices()
             trailing_vert_num, leading_vert_num = vert_perm[2], vert_perm[3]
             current_coeff = current_coeff * face_laurents[tet.face(2,leading_vert_num).index()]**sign
-            if coorientations[tet.index()][trailing_vert_num] == -1 and coorientations[tet.index()][leading_vert_num] == -1:
+
+            if (coorientations[tet.index()][trailing_vert_num] == -1 and
+                coorientations[tet.index()][leading_vert_num]  == -1):
                 # we are the top embed so:
                 tet_coeffs[tet.index()] = tet_coeffs[tet.index()] - current_coeff
                 sign = -1 # now we go down the right side
-            elif edge_colour == 'L' and tet in red_tetrahedra or edge_colour == 'R' and tet in blue_tetrahedra:
+            elif ((edge_colour == "L" and tet in red_tetrahedra) or
+                  (edge_colour == "R" and tet in blue_tetrahedra)): 
                 tet_coeffs[tet.index()] = tet_coeffs[tet.index()] - current_coeff
-            if verbose > 0: print 'current tet_coeffs', tet_coeffs
+            if verbose > 0:
+                print "current tet_coeffs", tet_coeffs
 
         ET_matrix.append(tet_coeffs)
 
@@ -123,7 +140,8 @@ def big_polynomial(tri, angle, alpha = True):
     # set up
     ZH = group_ring(tri, angle, alpha = alpha)
     P = ZH.polynomial_ring()
-    if verbose > 0: print 'angle', angle
+    if verbose > 0:
+        print "angle", angle
 
     ET = edges_to_tetrahedra_matrix(tri, angle, ZH, P)
     return normalise_poly(ET.determinant(), ZH, P)
@@ -131,9 +149,8 @@ def big_polynomial(tri, angle, alpha = True):
 
 # computing the small veering polynomial
 
-
 @liberal
-def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = 'veering'):
+def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = "veering"):
     # In mode alexander, we are computing the transpose of the
     # boundary operator from relative 1-chains to relative 0-chains of
     # the dual 2-complex (relative to its vertex set).  Note that we
@@ -142,29 +159,38 @@ def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = 'vee
     # a kernel.  By using all of the dual vertices we are splitting
     # off a free summand of the correct rank.
 
-    # In mode veering we are computing the matrix which assigns to a triangle the switch condition on its dual lower track
-    coorientations = is_transverse_taut(triangulation, angle_structure, return_type = 'tet_vert_coorientations')
-    if verbose > 0: print 'coorientations', coorientations
+    # In mode "veering" we are computing the matrix which assigns to a
+    # triangle the switch condition on its dual lower track
+    coorientations = is_transverse_taut(triangulation, angle_structure, return_type = "tet_vert_coorientations")
+    if verbose > 0:
+        print "coorientations", coorientations
     face_laurents = faces_in_laurent(triangulation, angle_structure, ZH)
-    if verbose > 0: print 'face_laurents', face_laurents
+    if verbose > 0:
+        print "face_laurents", face_laurents
 
     ET_matrix = [] # now to find the face coefficients relative to each edge
     for tet in triangulation.tetrahedra():
         # get its upper edge - we iterate over upper edges of tetrahedra
-        if verbose > 0: print 'tet_index', tet.index()
+        if verbose > 0:
+            print "tet_index", tet.index()
         edge = tet_lower_upper_edges(tet, coorientations)[1]
-        if verbose > 0: print 'edge_index', edge.index()
+        if verbose > 0:
+            print "edge_index", edge.index()
         embeddings = edge.embeddings()
 
         # find index of tet in the list of embeddings of edge
         for i, embed in enumerate(embeddings):
             tet = embed.tetrahedron()
-            if verbose > 0: print 'current_tet', tet.index()
+            if verbose > 0:
+                print "current_tet", tet.index()
             vert_perm = embed.vertices()
             trailing_vert_num, leading_vert_num = vert_perm[2], vert_perm[3]
-            if coorientations[tet.index()][trailing_vert_num] == +1 and coorientations[tet.index()][leading_vert_num] == +1:
+
+            if (coorientations[tet.index()][trailing_vert_num] == +1 and
+                coorientations[tet.index()][leading_vert_num]  == +1):
                 bottom_index = i
                 break
+
         # rotate
         embeddings = embeddings[bottom_index:] + embeddings[:bottom_index]
 
@@ -172,19 +198,20 @@ def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = 'vee
         sign = 1 # we are going up the left side of the edge
         current_coeff = ZH(1)
 
-        # because of a sign change, we install the first and last by hand.
         embed = embeddings[0]
         assert tet.index() == embed.tetrahedron().index() # sanity check
 
-        if mode == 'veering':
+        if mode == "veering":
+            # because of a sign change, we install the first and last by hand.
             vert_perm = embed.vertices()
             trailing_vert_num, leading_vert_num = vert_perm[2], vert_perm[3]
             leading_face = tet.triangle(trailing_vert_num)
             face_coeffs[leading_face.index()] = face_coeffs[leading_face.index()] + current_coeff
             trailing_face = tet.triangle(leading_vert_num)
             face_coeffs[trailing_face.index()] = face_coeffs[trailing_face.index()] + current_coeff
-            if verbose > 0: print 'face_coeffs', face_coeffs
-            embeddings = embeddings[1:-1] # so we can skip the first and last
+            if verbose > 0:
+                print "face_coeffs", face_coeffs
+            embeddings = embeddings[1:-1] # and remove them
 
         for embed in embeddings:
             tet = embed.tetrahedron()
@@ -192,22 +219,27 @@ def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = 'vee
             trailing_vert_num, leading_vert_num = vert_perm[2], vert_perm[3]
             leading_face = tet.triangle(trailing_vert_num)
             trailing_face = tet.triangle(leading_vert_num)
+
             if sign == 1:
                 use_face = trailing_face
             else: # sign == -1
                 use_face = leading_face
             current_coeff = current_coeff * (face_laurents[use_face.index()])**sign
-            if verbose > 0: print 'current_coeff', current_coeff
-            # have we reached the top?
-            if coorientations[tet.index()][trailing_vert_num] == -1 and coorientations[tet.index()][leading_vert_num] == -1: ## we are the top embed
-                sign = -1 # we are going down the right side of the edge
+            if verbose > 0:
+                print "current_coeff", current_coeff
+
+            if (coorientations[tet.index()][trailing_vert_num] == -1 and 
+                coorientations[tet.index()][leading_vert_num]  == -1): ## we are the top embed
+                sign = -1 # so now are going down the right side of the edge
                 current_coeff = current_coeff * (face_laurents[leading_face.index()])**sign
-                if verbose > 0: print 'top current_coeff', current_coeff
-            if mode == 'veering':
+                if verbose > 0:
+                    print "top current_coeff", current_coeff
+            if mode == "veering":
                 face_coeffs[leading_face.index()] = face_coeffs[leading_face.index()] - current_coeff
-            elif mode == 'alexander':
+            elif mode == "alexander":
                 face_coeffs[leading_face.index()] = face_coeffs[leading_face.index()] + sign*current_coeff
-            if verbose > 0: print 'face_coeffs', face_coeffs
+            if verbose > 0:
+                print "face_coeffs", face_coeffs
 
         ET_matrix.append(face_coeffs)
 
@@ -216,7 +248,7 @@ def edges_to_triangles_matrix(triangulation, angle_structure, ZH, P, mode = 'vee
 
 
 @liberal
-def small_polynomial(tri, angle, alpha = True, mode = 'veering'):
+def small_polynomial(tri, angle, alpha = True, mode = "veering"):
     # set up
     ZH = group_ring(tri, angle, alpha = alpha)
     P = ZH.polynomial_ring()
@@ -229,7 +261,7 @@ def small_polynomial(tri, angle, alpha = True, mode = 'veering'):
 
 
 @liberal
-def small_polynomial_via_tree(tri, angle, alpha = True, mode = 'veering'):
+def small_polynomial_via_tree(tri, angle, alpha = True, mode = "veering"):
     # set up
     ZH = group_ring(tri, angle, alpha = alpha)
     P = ZH.polynomial_ring()
@@ -246,7 +278,7 @@ def small_polynomial_via_tree(tri, angle, alpha = True, mode = 'veering'):
 
 
 @liberal
-def small_polynomial_via_smith(tri, angle, alpha = True, mode = 'veering'):
+def small_polynomial_via_smith(tri, angle, alpha = True, mode = "veering"):
     # set up
     assert tri.homology().rank() == 1 # need the polynomial ring to be a PID
     ZH = group_ring(tri, angle, alpha = alpha, ring = QQ) # ditto
@@ -262,7 +294,7 @@ def small_polynomial_via_smith(tri, angle, alpha = True, mode = 'veering'):
 
 
 @liberal
-def small_polynomial_via_tree_and_smith(tri, angle, alpha = True, mode = 'veering'):
+def small_polynomial_via_tree_and_smith(tri, angle, alpha = True, mode = "veering"):
     # set up
     assert tri.homology().rank() == 1 # need the polynomial ring to be a PID
     ZH = group_ring(tri, angle, alpha = alpha, ring = QQ) # ditto
@@ -284,7 +316,7 @@ def small_polynomial_via_tree_and_smith(tri, angle, alpha = True, mode = 'veerin
 # Remarks on small_polynomial_via_smith and
 # small_polynomial_via_tree_and_smith
 
-# 1. Running on 'gLLAQacdefefjkaaqks_200210' appears to involve very
+# 1. Running on "gLLAQacdefefjkaaqks_200210" appears to involve very
 # large intermediate computations - I've never waited long enough for
 # the function to actually return.  So there is a serious dichotomy:
 # the version using smith normal form is exponentially faster most of
