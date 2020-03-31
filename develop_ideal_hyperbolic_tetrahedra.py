3fe641e410d69b25fc038c128ae9b738f797000c
Saul Schleimer <saulsch@gmail.com>
2020-03-31 15:39:51 +0100

    cleaned up the develop function

--- develop_ideal_hyperbolic_tetrahedra.py
+++ develop_ideal_hyperbolic_tetrahedra.py
@@ -2,19 +2,16 @@ from basic_math import matrix, CP1
 
 def developed_position(A1,A2,A3,z): #use Feng's "solving Thurston's equations in a commutative ring"
     # print A1, A2, A3, z
+    epsilon = 0.00000001
     a1,b1 = A1 ## eg 1,0
     a2,b2 = A2 ## eg 0,1
     a3,b3 = A3 ## eg 1,1
-    assert (a1*b2-b1*a2) != complex(0,0), ",".join([str(a1), str(a2), str(b1), str(b2)]) 
-    invdet = 1.0/(a1*b2-b1*a2) ## eg 1
-    # X = [[b2*invdet, -a2*invdet],[-b1*invdet, a1*invdet]] ## eg [[1,0],[0,1]]
-    X = matrix((b2*invdet, -a2*invdet, -b1*invdet, a1*invdet))
-    # [a3p,b3p] = matrix_mult_vector(X,[a3,b3]) ## eg [1,1]
+    Xinv = matrix((a1, a2, b1, b2))
+    assert abs(Xinv.det()) > epsilon, Xinv
+    X = Xinv.inverse()
     a3p, b3p = X * A3
-    # Xinv = matrix2_inv(X)
-    assert abs(b3p) > 0.00000001 and abs(a3p) > 0.00000001, str(b3p) + ' ' + str(a3p) + ' ' + str([A1,A2,A3])
-    # out = matrix_mult_vector(X.inverse(), [-z/b3p,-1/a3p]) ## eg [-z,-1] = [z,1]
-    out = X.inverse() * CP1((-z/b3p,-1/a3p))
+    assert abs(b3p) > epsilon and abs(a3p) > epsilon, X * A3
+    out = Xinv * CP1((-z/b3p,-1/a3p))
     return out.preferred_rep() 
 
 unknown_vert_to_known_verts_ordering = {0:(3,2,1), 1:(2,3,0), 2:(1,0,3), 3:(0,1,2)}
