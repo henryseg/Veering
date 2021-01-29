3e4900037c40f82bb8be20c4d7d331a29baa5b2c
Saul Schleimer <saulsch@gmail.com>
2021-01-29 16:14:41 +0000

    short script to search the census

---
+++ veering_census_search.py
@@ -0,0 +1,19 @@
+from file_io import parse_data_file, write_data_file
+
+lines = parse_data_file('Data/veering_census_with_data.txt')
+
+out = []
+for line in lines:
+    data = line.split(" ")
+    sig = data[0]
+    edge_ori = data[4]
+    ladder_counts = data[6]
+    ladder_counts = ladder_counts[1:-1]
+    ladder_counts = ladder_counts.split(",")
+    ladder_counts = [int(a) for a in ladder_counts]
+    ladders_even = all(a % 4 == 0 for a in ladder_counts)
+    if edge_ori == 'N' and ladders_even:
+        print(sig, edge_ori, ladder_counts)
+        out.append([sig])
+
+write_data_file(out, 'non_ori_ladders_even.txt') 
