88fa69ce8a002e990020edd0b43474ccc0e96433
Saul Schleimer <saulsch@gmail.com>
2021-01-30 16:20:43 +0000

    for searching the veering census plus data

--- veering_census_search.py
+++ veering_census_search.py
@@ -7,13 +7,14 @@ for line in lines:
     data = line.split(" ")
     sig = data[0]
     edge_ori = data[4]
+    euler_ord = data[5]
     ladder_counts = data[6]
     ladder_counts = ladder_counts[1:-1]
     ladder_counts = ladder_counts.split(",")
     ladder_counts = [int(a) for a in ladder_counts]
     ladders_even = all(a % 4 == 0 for a in ladder_counts)
     if edge_ori == 'N' and ladders_even:
-        print(sig, edge_ori, ladder_counts)
+        # print(sig, edge_ori, ladder_counts)
         out.append([sig])
 
-write_data_file(out, 'non_ori_ladders_even.txt') 
+write_data_file(out, 'foo.txt') 
