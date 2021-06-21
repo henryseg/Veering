e102292bb0e2851d5784ed1d5f701063f38c3045
Saul Schleimer <saulsch@gmail.com>
2021-06-21 15:38:49 +0100

    testing lattice equality

--- test_suite.py
+++ test_suite.py
@@ -66,7 +66,7 @@ def run_tests(num_to_check=1000):
             excised_tri, _ = veering_fan_excision.excise_fans(tri, angle)
             assert excised_tri.isIsomorphicTo(m003) != None or excised_tri.isIsomorphicTo(m004) != None
 
-    print("all tests depending on regina passed")
+    print("all basic tests passed")
 
     try:
         import snappy
@@ -356,7 +356,21 @@ def run_tests(num_to_check=1000):
 
     if sage_working:
         from sage.combinat.words.word_generators import words
+        from sage.modules.free_module_integer import IntegerLattice
         import z_charges
+        import regina
+        for i in range(3):
+            sig = "b+-LR" + str(words.RandomWord(8, 2, "LR"))  # 8 is a magic number
+            print("testing lattice for punc torus bundle", sig)
+            M = snappy.Manifold(sig)
+            tri = regina.Triangulation3(M)
+            t, A = z_charges.sol_and_kernel(M)
+            B = z_charges.leading_trailing_deformations(tri)
+            AA = IntegerLattice(A)
+            BB = IntegerLattice(B)
+            assert AA == BB.saturation()
+            
+    if sage_working:
         for i in range(3):
             sig = "b++LR" + str(words.RandomWord(8, 2, "LR"))  # 8 is a magic number
             print("testing charges for punc torus bundle", sig)
@@ -369,7 +383,7 @@ def run_tests(num_to_check=1000):
             print("testing charges for punc torus bundle", sig)
             M = snappy.Manifold(sig)
             assert z_charges.can_deal_with_reduced_angles(M)
-    
+            
     if sage_working:
         print("all tests depending on sage passed")
 
