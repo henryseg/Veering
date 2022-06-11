ccde02bfde51b3e1f8d3a763e619cb80138dd202
Henry Segerman <github.com@segerman.org>
2022-06-10 21:23:34 -0700

    start to factor moving and expanding a continent along a flow cycle

--- draw_continent_circle.py
+++ draw_continent_circle.py
@@ -723,6 +723,66 @@ def flow_edge_in_continent(con_tet, edge_num):
     assert edge != None
     return edge
 
+def go_up_flow(flow_cycle, con, flow_edges, flow_tetrahedra, upwards_flow_index):
+    edge = flow_edges[-1]
+    last_tet = None
+    while True:
+        con.update_boundary()  
+        upper_boundary_triangles = [t for t in edge.boundary_triangles if t.is_upper] 
+        if len(upper_boundary_triangles) == 0:
+            break ## out of while loop
+        last_tet = con.bury(upper_boundary_triangles[0])
+    assert last_tet != None
+    upwards_flow_index = (upwards_flow_index + 1) % len(flow_cycle)
+    assert last_tet.index == flow_cycle[upwards_flow_index][0]
+    flow_tetrahedra.append(last_tet)
+    flow_edges.append(flow_edge_in_continent(last_tet, flow_cycle[upwards_flow_index][1]))
+    con.make_convex() ### could this take us further up dual_cycle? We don't think so
+    return upwards_flow_index
+
+def go_down_flow(flow_cycle, con, flow_edges, flow_tetrahedra, downwards_flow_index):
+    tet = flow_tetrahedra[0]
+    edge = tet.lower_edge()
+    flow_edges.insert(0, edge) 
+    ### now build the continent to get new_lowest_tet
+    vt = con.vt
+    manifold_edge = vt.tri.edge(edge.index)
+    downwards_flow_index = (downwards_flow_index - 1) % len(flow_cycle)
+    ### is the flow cycle vertical through the tetrahedron? 
+    tet_below = get_tet_above_edge(vt.tri, vt.angle, manifold_edge, tet_vert_coorientations = vt.coorientations, get_tet_below_edge = True)
+    tet_below_num = tet_below.index()
+    top_vert_nums = get_tet_top_vert_nums(vt.coorientations, tet_below_num)
+    top_edge_num = vert_pair_to_edge_num[tuple(top_vert_nums)]
+
+    if (tet_below_num, top_edge_num) == flow_cycle[downwards_flow_index]: ### flow cycle went straight up
+        while True:
+            con.update_boundary()  
+            lower_boundary_triangles = [t for t in edge.boundary_triangles if not t.is_upper] 
+            if len(lower_boundary_triangles) == 0:
+                break ## out of while loop
+            last_tet = con.bury(lower_boundary_triangles[0])
+    else:
+        ### find which side of the edge our tet is in
+        # print('edge index', edge.index)
+        side_tet_collections_at_edge = vt.side_tet_collections[edge.index] ## index in the manifold
+        side_face_collections_at_edge = vt.side_face_collections[edge.index]
+        downward_path = None
+        flow_step = flow_cycle[downwards_flow_index]
+        for i, side_tet_collection in enumerate(side_tet_collections_at_edge):
+            if flow_step in side_tet_collection:
+                downward_path = side_tet_collection[:side_tet_collection.index(flow_step) + 1]
+                downward_path_faces = side_face_collections_at_edge[i][:side_tet_collection.index(flow_step) + 1]
+        assert downward_path != None
+        for j, (tet_num, edge_num) in enumerate(downward_path):
+            con.update_boundary()  
+            lower_boundary_triangles = [t for t in edge.boundary_triangles if not t.is_upper and t.index == downward_path_faces[j][0]] 
+            assert len(lower_boundary_triangles) == 1
+            last_tet = con.bury(lower_boundary_triangles[0])
+    assert last_tet != None
+    flow_tetrahedra.insert(0, last_tet)
+    con.make_convex() ### could this take us further down dual_cycle? We don't think so
+    return downwards_flow_index
+
 def make_continent_drill_flow_cycle(veering_isosig, flow_cycle, num_steps):
     tri, angle = isosig_to_tri_angle(veering_isosig)
     vt = veering_triangulation(tri, angle) #, tet_shapes = tet_shapes)
@@ -733,7 +793,8 @@ def make_continent_drill_flow_cycle(veering_isosig, flow_cycle, num_steps):
     initial_tet_face = tet_face(vt, tet_0, face_0, verts_pos = [None, None, None, None])
     con = continent( vt, initial_tet_face)
 
-    side_face_collections, side_tet_collections = edge_side_face_collections(vt.tri, vt.angle, tet_vert_coorientations = vt.coorientations, return_tets = True, order_bottom_to_top = False)
+    ## install into vt:
+    vt.side_face_collections, vt.side_tet_collections = edge_side_face_collections(vt.tri, vt.angle, tet_vert_coorientations = vt.coorientations, return_tets = True, order_bottom_to_top = False)
     # print('sfc', side_face_collections)
     # print('stc', side_tet_collections)
     ### identify the next edge in the cycle 
@@ -748,63 +809,11 @@ def make_continent_drill_flow_cycle(veering_isosig, flow_cycle, num_steps):
     upwards_flow_index = 0
     downwards_flow_index = 0
     for i in range(num_steps):
-        # print(i)
         if i%2 == 0: ### go up
-            edge = flow_edges[-1]
-            last_tet = None
-            while True:
-                con.update_boundary()  
-                upper_boundary_triangles = [t for t in edge.boundary_triangles if t.is_upper] 
-                if len(upper_boundary_triangles) == 0:
-                    break ## out of while loop
-                last_tet = con.bury(upper_boundary_triangles[0])
-            assert last_tet != None
-            upwards_flow_index = (upwards_flow_index + 1) % len(flow_cycle)
-            assert last_tet.index == flow_cycle[upwards_flow_index][0]
-            flow_tetrahedra.append(last_tet)
-            flow_edges.append(flow_edge_in_continent(last_tet, flow_cycle[upwards_flow_index][1]))
-            con.make_convex() ### could this take us further up dual_cycle? We don't think so
+            upwards_flow_index = go_up_flow(flow_cycle, con, flow_edges, flow_tetrahedra, upwards_flow_index)
+
         else: ### go down
-            tet = flow_tetrahedra[0]
-            edge = tet.lower_edge()
-            flow_edges = [edge] + flow_edges
-            ### now build the continent to get new_lowest_tet
-            manifold_edge = tri.edge(edge.index)
-            downwards_flow_index = (downwards_flow_index - 1) % len(flow_cycle)
-            ### is the flow cycle vertical through the tetrahedron? 
-            tet_below = get_tet_above_edge(vt.tri, vt.angle, manifold_edge, tet_vert_coorientations = vt.coorientations, get_tet_below_edge = True)
-            tet_below_num = tet_below.index()
-            top_vert_nums = get_tet_top_vert_nums(vt.coorientations, tet_below_num)
-            top_edge_num = vert_pair_to_edge_num[tuple(top_vert_nums)]
-
-            if (tet_below_num, top_edge_num) == flow_cycle[downwards_flow_index]: ### flow cycle went straight up
-                while True:
-                    con.update_boundary()  
-                    lower_boundary_triangles = [t for t in edge.boundary_triangles if not t.is_upper] 
-                    if len(lower_boundary_triangles) == 0:
-                        break ## out of while loop
-                    last_tet = con.bury(lower_boundary_triangles[0])
-            else:
-                ### find which side of the edge our tet is in
-                # print('edge index', edge.index)
-                side_tet_collections_at_edge = side_tet_collections[edge.index] ## index in the manifold
-                side_face_collections_at_edge = side_face_collections[edge.index]
-                downward_path = None
-                flow_step = flow_cycle[downwards_flow_index]
-                for i, side_tet_collection in enumerate(side_tet_collections_at_edge):
-                    if flow_step in side_tet_collection:
-                        downward_path = side_tet_collection[:side_tet_collection.index(flow_step) + 1]
-                        downward_path_faces = side_face_collections_at_edge[i][:side_tet_collection.index(flow_step) + 1]
-                assert downward_path != None
-                for j, (tet_num, edge_num) in enumerate(downward_path):
-                    con.update_boundary()  
-                    lower_boundary_triangles = [t for t in edge.boundary_triangles if not t.is_upper and t.index == downward_path_faces[j][0]] 
-                    assert len(lower_boundary_triangles) == 1
-                    last_tet = con.bury(lower_boundary_triangles[0])
-            assert last_tet != None
-            flow_tetrahedra = [last_tet] + flow_tetrahedra
-            con.make_convex() ### could this take us further down dual_cycle? We don't think so
-
+            downwards_flow_index = go_down_flow(flow_cycle, con, flow_edges, flow_tetrahedra, downwards_flow_index)
     con.update_boundary()
     return con, flow_tetrahedra, flow_edges
 
