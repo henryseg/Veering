45c4025737d2c3dc9d6a8b811e7684b878f13520
Saul Schleimer <saulsch@gmail.com>
2022-09-07 08:23:00 +0100

    pulled code out of fund_domain to get test suite to run

--- veering/fundamental_domain.py
+++ veering/fundamental_domain.py
@@ -2,10 +2,8 @@
 # fundamental_domain.py
 #
 
-# Computation of loops in pi_1(M) by building a fundamental domain
-# realised as a spanning tree in the dual 1 skeleton.
-
-from sage.groups.free_group import FreeGroup
+# Building a spanning tree for, and computation of loops, in the dual
+# one-skeleton.
 
 from .taut import liberal
 from .transverse_taut import is_transverse_taut, edge_side_face_collections
@@ -174,64 +172,3 @@ def non_tree_face_cycles(tri, angle):
             cycle[face] = sign
         cycles.append(cycle)
     return cycles
-
-
-@liberal
-def fundamental_group(tri, angle, simplified = True):
-    """
-    Returns the finite presentation of the fundamental group arising
-    from the canonical spanning tree.
-    
-    Input 
-
-    * "tri" -- a regina triangulation.
-
-    * "angle" -- an veering angle structure
-
-    * "simplified" -- returns the Tietze simpification [Havas, 1994]
-      of the fundamental group.
-
-    Examples:
-
-    sage: sig = "cPcbbbdxm_10"  
-    sage: fundamental_group(sig, False)
-    Finitely presented group < x0, x1, x2, x3 | x1^-1*x0^-1*x1^-1*x3*x2*x3, x2^-1*x1^-1*x2^-1*x0*x3*x0, x0 >
-
-    This is the unsimplified presentation of the fundamental group of
-    the figure-eight sibling.  Note that we are using the liberal
-    decorator here.
-
-    sage: sig = "cPcbbbiht_12"  # figure-eight
-    sage: fundamental_group(sig)
-    Finitely presented group < x1, x2 | x1^-1*x2^-2*x1^-1*x2*x1*x2^-1*x1*x2 >
-    
-    This is the simplified presentation of the fundamental group of
-    the figure-eight knot complement.
-    """
-    
-    tree = spanning_dual_tree(tri)
-    tree_faces = tree[0]
-    F = FreeGroup(tri.countTriangles())
-    # We add all of the faces as generators.  This is useful, for
-    # example, when defining homomorphisms from \pi_1(M).
-    gens = F.gens()
-
-    esfc = edge_side_face_collections(tri, angle)  # can this be done with just the routines in fundamental_domain?
-    relators = []
-    for edge in esfc:  # relations for the edges
-        rel = F.one()
-        left, right = edge
-        left.reverse()
-        for term in left:
-            rel = rel * gens[term[0]].inverse()
-        for term in right:
-            rel = rel * gens[term[0]]
-        relators.append(rel)
-
-    gens = F.gens()
-    for f in tree_faces:  # killing the tree faces
-        relators.append(gens[f])
-    G = F/relators
-    if simplified:
-        G = G.simplified()
-    return G
