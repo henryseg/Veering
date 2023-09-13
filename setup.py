7cefbf1d809fc6b9b0202917b6b953515083e996
Saul Schleimer <saulsch@gmail.com>
2023-09-13 15:31:05 +0200

    version - updated to 0.3 in local repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(name='veering',
-      version='0.2',
+      version='0.3',
       description='Taut and veering triangulations',
       long_description=long_description,
       long_description_content_type='text/markdown',
--- veering/__init__.py
+++ veering/__init__.py
@@ -1,1 +1,6 @@
+#
+# __init__.py
+#
+
+version = "0.3" 
 from .file_io import veering_census
